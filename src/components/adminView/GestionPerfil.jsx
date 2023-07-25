e205d280d2afca85ae157a7c69a241a89621a28d
Telnesito <carlosj_1703@hotmail.com>
2023-07-25 15:03:13 -0400

    form-update-profile

--- src/components/adminView/GestionPerfil.jsx
+++ src/components/adminView/GestionPerfil.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import NavBar from "./NavBar";
 
 const GestionPerfil = () => {
@@ -11,14 +11,70 @@ const GestionPerfil = () => {
     >
       <NavBar title={"Gestionar perfil"} />
       <Box
-        width={"80%"}
+        width={"100%"}
         minHeight={"500px"}
         height={"100vh"}
         bgcolor={"var(--backgroundColor)"}
         display={"flex"}
         justifyContent={"center"}
-        alignItems={"center"}
-      ></Box>
+        alignItems={"flex-start"}
+      >
+        <Box
+          mt={"20px"}
+          display={"flex"}
+          justifyContent={"center"}
+          width={"80%"}
+          flexDirection={"column"}
+          gap={"15px"}
+        >
+          <Typography fontSize={"20px"} fontFamily={"Poppins"} color={"black"}>
+            Actualizar perfil
+          </Typography>
+          <Typography color={"black"}>
+            Ingresar los nuevos dato de la cuenta administrador
+          </Typography>
+          <TextField
+            helperText="Asegurese de agregar un correo electronico valido, si es incorrecto no podrá recuperar la contraseña en un futuro"
+            sx={
+              {
+                //   width: "40%",
+              }
+            }
+            label="Correo electronico"
+            required
+            type="email"
+            variant="filled"
+          ></TextField>
+          <TextField
+            helperText="La contraseña debe ser mayor a 5 digitos"
+            sx={
+              {
+                //   width: "40%",
+              }
+            }
+            label="Contraseña"
+            type="password"
+            required
+            variant="filled"
+          ></TextField>
+          <TextField
+            type="password"
+            sx={
+              {
+                //   width: "40%",
+              }
+            }
+            label="Repetir contraseña "
+            required
+            variant="filled"
+            helperText="Repetir contraseña"
+          ></TextField>
+
+          <Button color="primary" variant="contained">
+            Actualizar
+          </Button>
+        </Box>
+      </Box>
     </Box>
   );
 };
